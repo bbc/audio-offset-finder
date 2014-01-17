44f28dceb1f42101f425b131f50dd27616682dad
Yves Raimond <yves.raimond@bbc.co.uk>
2014-01-17 16:57:34 +0000

    More parametrisation

--- offset_finder.py
+++ offset_finder.py
@@ -3,22 +3,24 @@
 from subprocess import Popen
 from scipy.io import wavfile
 from scikits.talkbox.features.mfcc import mfcc
-import tempfile
+import os, tempfile
 import numpy as np
 
-def find_offset(file1, file2):
-    tmp1 = convert_and_trim(file1)
-    tmp2 = convert_and_trim(file2)
+def find_offset(file1, file2, fs=16000, trim=60*15, correl_nframes=1000):
+    tmp1 = convert_and_trim(file1, fs, trim)
+    tmp2 = convert_and_trim(file2, fs, trim)
     a1 = wavfile.read(tmp1)[1] / (2.0 ** 15)
     a2 = wavfile.read(tmp2)[1] / (2.0 ** 15)
+    os.remove(tmp1)
+    os.remove(tmp2)
     mfcc1 = mfcc(a1, nwin=256, nfft=512, fs=16000, nceps=13)[0]
     mfcc2 = mfcc(a2, nwin=256, nfft=512, fs=16000, nceps=13)[0]
-    c = cross_correlation(mfcc1, mfcc2)
+    c = cross_correlation(mfcc1, mfcc2, nframes=correl_nframes)
     max_k_index = np.argmax(c)
-    offset = max_k_index * 160.0 / 16000.0 # * over / sample rate
+    offset = max_k_index * 160.0 / float(fs) # * over / sample rate
     return offset
 
-def cross_correlation(mfcc1, mfcc2, nframes=1000):
+def cross_correlation(mfcc1, mfcc2, nframes):
     mfcc1 = std_mfcc(mfcc1)
     mfcc2 = std_mfcc(mfcc2)
     n1, mdim1 = mfcc1.shape
@@ -33,10 +35,10 @@ def cross_correlation(mfcc1, mfcc2, nframes=1000):
 def std_mfcc(mfcc):
     return (mfcc - np.mean(mfcc, axis=0)) / (np.max(mfcc,axis=0) - np.min(mfcc,axis=0))
 
-def convert_and_trim(file1):
-    tmp1 = tempfile.NamedTemporaryFile(mode='r+b', prefix='offset_', suffix='.wav')
-    tmp1_name = tmp1.name
-    tmp1.close()
-    psox = Popen(['sox', file1, '-c', '1', '-r', '16000', '-b', '16', tmp1_name, 'trim', '0', str(60 * 15)])
+def convert_and_trim(afile, fs, trim):
+    tmp = tempfile.NamedTemporaryFile(mode='r+b', prefix='offset_', suffix='.wav')
+    tmp_name = tmp.name
+    tmp.close()
+    psox = Popen(['sox', afile, '-c', '1', '-r', str(fs), '-b', '16', tmp_name, 'trim', '0', str(trim)])
     psox.wait()
-    return tmp1_name
+    return tmp_name
