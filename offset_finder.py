279bce7a2242f32fb4b94e4662ee2be0f077d3d9
Yves Raimond <yves.raimond@bbc.co.uk>
2014-01-24 13:04:30 +0000

    Adding headers

--- offset_finder.py
+++ offset_finder.py
@@ -1,4 +1,18 @@
-#!/usr/bin/python
+# offset-finder
+#
+# Copyright (c) 2014 British Broadcasting Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from subprocess import Popen
 from scipy.io import wavfile
--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python
 
+# offset-finder
+#
+# Copyright (c) 2014 British Broadcasting Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from setuptools import setup
 
 setup(
     name='offset-finder',
-    version='0.0.4',
+    version='0.1.0',
     description='Find offset of audio file within another audio file',
     author='Yves Raimond',
     author_email='yves.raimond@bbc.co.uk',
