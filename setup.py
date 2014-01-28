3f2a1e7155fd2fe5a3cc4ff717905477acd2b02f
Yves Raimond <yves.raimond@bbc.co.uk>
2014-01-28 15:07:16 +0000

    Prodding description

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup
 setup(
     name='audio-offset-finder',
     version='0.1.0',
-    description='Find offset of audio file within another audio file',
+    description='Find the offset of an audio file within another audio file',
     author='Yves Raimond',
     author_email='yves.raimond@bbc.co.uk',
     url='https://github.com/bbcrd/audio-offset-finder',
