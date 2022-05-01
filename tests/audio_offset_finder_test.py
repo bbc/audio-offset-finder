7539b1dbc786b76740f0f68d7bb83fc9f9d2dafb
Stephen Jolly <stephen.jolly@bbc.co.uk>
2022-05-01 13:32:18 +0100

    Port tests to pytest
    Nose is now rather old, and no longer actively supported.s

--- tests/audio_offset_finder_test.py
+++ tests/audio_offset_finder_test.py
@@ -14,9 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from nose.tools import *
+import pytest
 from audio_offset_finder.audio_offset_finder import *
-from numpy.testing import *
+#from numpy.testing import *
 from io import StringIO
 from unittest.mock import patch
 import numpy as np
@@ -25,6 +25,7 @@ from importlib.machinery import ModuleSpec, SourceFileLoader
 from importlib.util import spec_from_loader, module_from_spec
 import argparse
 
+#Function to import code from a file
 def import_from_source( name : str, file_path : str ) -> types.ModuleType:
     loader : SourceFileLoader = SourceFileLoader(name, file_path)
     spec : ModuleSpec = spec_from_loader(loader.name, loader)
@@ -46,45 +47,51 @@ def test_find_offset():
     # timbl_2.mp3: File truncated at 12.254 seconds
     # timbl_3.mp3: File truncated at 12.223 seconds with white noise added to it
     results = find_offset(path('timbl_1.mp3'), path('timbl_2.mp3'), hop_length=160, trim=35) 
-    assert_almost_equal(results["offset"], 12.26)
-    assert(results["score"] > 10)
+    assert pytest.approx(results["offset"]) == 12.26
+    assert results["score"] > 10
+
     results = find_offset(path('timbl_1.mp3'), path('timbl_3.mp3'), hop_length=160, trim=35)
-    assert_almost_equal(results["offset"], 12.24)
-    assert(results["score"] > 10)
+    assert pytest.approx(results["offset"]) == 12.24
+    assert results["score"] > 10
+
     results = find_offset(path('timbl_1.mp3'), path('timbl_1.mp3'), hop_length=160, trim=35) 
-    assert_almost_equal(results["offset"], 0.0)
-    assert(results["score"] > 10)
+    assert pytest.approx(results["offset"]) == 0.0
+    assert results["score"] > 10
+
     results = find_offset(path('timbl_2.mp3'), path('timbl_2.mp3'), hop_length=160, trim=35) 
-    assert_almost_equal(results["offset"], 0.0)
-    assert(results["score"] > 10)
+    assert pytest.approx(results["offset"]) == 0.0
+    assert results["score"] > 10
+
     results = find_offset(path('timbl_2.mp3'), path('timbl_1.mp3'), hop_length=160, trim=35) 
-    assert(results["score"] < 10) # No good results["offset"] found
+    assert results["score"] < 10 # No good results["offset"] found
+
     results = find_offset(path('timbl_1.mp3'), path('timbl_2.mp3'), hop_length=160, trim=1) 
-    assert(results["score"] < 10) # No good results["offset"] found
-    with assert_raises(InsufficientAudioException):
-        results = find_offset(path('timbl_1.mp3'), path('timbl_2.mp3'), hop_length=160, trim=0.1)
+    assert results["score"] < 10 # No good results["offset"] found
+
+    with pytest.raises(InsufficientAudioException):
+        find_offset(path('timbl_1.mp3'), path('timbl_2.mp3'), hop_length=160, trim=0.1)
 
 def test_ensure_non_zero():
     signal = np.zeros(100)
-    assert_equal(len(np.where(signal == 0)[0]), 100)
-    assert_equal(len(np.where(ensure_non_zero(signal) == 0)[0]), 0)
+    assert len(np.where(signal == 0)[0]) == 100
+    assert len(np.where(ensure_non_zero(signal) == 0)[0]) == 0
 
 def test_std_mfcc():
     m = np.array([[2,3,4], [4,5,5]])
     s1 = np.std([2,4])
     s2 = np.std([3,5])
     s3 = np.std([4,5])
-    assert_array_equal(std_mfcc(m), np.array([[-1.0/s1, -1.0/s2, -0.5/s3], [1.0/s1, 1.0/s2, 0.5/s3]]))
+    np.testing.assert_array_equal(std_mfcc(m), np.array([[-1.0/s1, -1.0/s2, -0.5/s3], [1.0/s1, 1.0/s2, 0.5/s3]]))
 
 def test_cross_correlation():
     m1 = np.array([[-0.5, -0.4, -0.4], [0.5, 0.5, 0.4], [0.1, -0.1, 0.1]])
     m2 = m1[1:]
     c = cross_correlation(m1, m2, 2)
-    assert_equal(np.argmax(c), 1)
+    assert np.argmax(c) == 1
 
     m2 = m1
     c = cross_correlation(m1, m2, 2)
-    assert_equal(np.argmax(c), 0)
+    assert np.argmax(c) == 0
 
 def test_tool():
     tool : types.ModuleType = import_from_source("audio-offset-finder", script_path)
@@ -92,6 +99,6 @@ def test_tool():
     with patch('sys.stdout', new=StringIO()) as fakeStdout:
         tool.main(args)
         output = fakeStdout.getvalue().strip()
-        assert_true(output)
-        assert_in("ffset: 12.26", output)
-        assert_in("core: 21.10", output)
+        assert output, "audio_offset_finder did not produce any output"
+        assert "ffset: 12.26" in output
+        assert "core: 21.10" in output
